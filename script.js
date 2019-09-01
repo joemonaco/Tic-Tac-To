168e1672bccf2a6cc87956b8a33bf7151b45ed42
Joe Monaco <s1049491@monmouth.edu>
2019-09-01 17:06:51 -0400

    mvp done

--- script.js
+++ script.js
@@ -49,17 +49,8 @@ function startGame() {
   heading.innerHTML = usersName + "'s turn!(" + userLetter + ")";
 }
 
-var boxes = [
-  "r1c1",
-  "r1c2",
-  "r1c3",
-  "r2c1",
-  "r2c2",
-  "r2c3",
-  "r3c1",
-  "r3c2",
-  "r3c3"
-];
+var boxes = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var placed = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]];
 
 //Turn 0 is Players turn
 var turn = 0;
@@ -67,29 +58,129 @@ var turn = 0;
 var userBoxes = [];
 var compBoxes = [];
 
-function boxClicked(id) {
+function boxClicked(id, row, col) {
   var box = document.getElementById(id);
   if (turn === 0) {
     if (!box.classList.contains("filled")) {
       box.classList.add(userLetter + "-added");
       box.classList.add("filled");
+
       let index = boxes.indexOf(id);
       userBoxes.push(boxes[index]);
+
+      placed[row][col] = userLetter;
+
       boxes.splice(index, 1);
-      turn = 1;
-      aiTurn();
+
+      if (!checkWinner()) {
+        turn = 1;
+        heading.innerHTML = "AI Turn";
+        aiTurn();
+      } else {
+        endGame();
+      }
     }
   }
 }
 
 function aiTurn() {
-  if (boxes.length != 0) {
-    let randomIndex = Math.floor(Math.random() * (boxes.length - 1));
-    var box = document.getElementById(boxes[randomIndex]);
-    box.classList.add("filled");
-    box.classList.add(compLetter + "-added");
-    compBoxes.push(boxes[randomIndex]);
-    boxes.splice(randomIndex, 1);
-    turn = 0;
+  setTimeout(function() {
+    if (boxes.length != 0) {
+      let randomIndex = Math.floor(Math.random() * (boxes.length - 1));
+      var box = document.getElementById(boxes[randomIndex]);
+      box.classList.add("filled");
+      box.classList.add(compLetter + "-added");
+      compBoxes.push(boxes[randomIndex]);
+      boxes.splice(randomIndex, 1);
+      if (!checkWinner()) {
+        turn = 0;
+        heading.innerHTML = usersName + "'s turn!(" + userLetter + ")";
+      } else {
+        endGame();
+      }
+    }
+  }, 500);
+}
+
+function checkBoxes() {
+  //Rows
+  if (placed[0][0] == placed[0][1] && placed[0][1] == placed[0][2]) {
+    return placed[0][0];
   }
+  if (placed[1][0] == placed[1][1] && placed[1][1] == placed[1][2]) {
+    return placed[1][0];
+  }
+  if (placed[2][0] == placed[2][1] && placed[2][1] == placed[2][2]) {
+    return placed[2][0];
+  }
+  //columns
+  if (placed[0][0] == placed[1][0] && placed[1][0] == placed[2][0]) {
+    return placed[0][0];
+  }
+  if (placed[0][1] == placed[1][1] && placed[1][1] == placed[2][1]) {
+    return placed[0][1];
+  }
+  if (placed[0][2] == placed[1][2] && placed[1][2] == placed[2][2]) {
+    return placed[0][2];
+  }
+  //Diagnol
+  if (placed[0][0] == placed[1][1] && placed[1][1] == placed[2][2]) {
+    return placed[0][0];
+  }
+  if (placed[0][2] == placed[1][1] && placed[1][1] == placed[0][2]) {
+    return placed[0][2];
+  }
+
+  if (boxes.length == 0) {
+    return "t";
+  } else {
+    return "not yet";
+  }
+}
+
+function checkWinner() {
+  let winner = checkBoxes();
+
+  if (winner === userLetter) {
+    heading.innerHTML = usersName + " Has Won!";
+
+    return true;
+  } else if (winner === compLetter) {
+    heading.innerHTML = "AI Has Won!";
+    return true;
+  } else if (winner === "t") {
+    heading.innerHTML == "TIE GAME!";
+    return true;
+  }
+  return false;
+}
+
+function endGame() {
+  setTimeout(function() {
+    var gameArea = document.getElementById("game-area");
+    gameArea.classList.add("hidden");
+
+    var newGameArea = document.getElementById("new-game");
+    newGameArea.classList.remove("hidden");
+
+    boxes = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    placed = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]];
+
+    for (var i = 0; i < 9; i++) {
+      let box = document.getElementById(boxes[i]);
+      console.log(boxes[i]);
+      if (box.classList.contains("x-added")) {
+        box.classList.remove("x-added");
+        box.classList.remove("filled");
+      }
+      if (box.classList.contains("o-added")) {
+        box.classList.remove("o-added");
+        box.classList.remove("filled");
+      }
+    }
+    heading.innerHTML = "Tic-Tac-Toe";
+
+    compBoxes = [];
+    userBoxes = [];
+  }, 1000);
 }
